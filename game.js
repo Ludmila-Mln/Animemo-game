f154be41bcd218ee8187b49f2f672e3bde36c11e
Ludmila-Mln <85869652+Ludmila-Mln@users.noreply.github.com>
2021-09-28 17:25:26 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+
 let cardArray = [ 
     { name: "bird", img: "images/bird.JPG", }, 
     { name: "bird", img: "images/bird.JPG", }, 
@@ -11,10 +12,14 @@ let cardArray = [
     { name: "zebra", img: "images/zebra.JPG", },
     { name: "leopard", img: "images/leopard.JPG", },
     { name: "leopard", img: "images/leopard.JPG", },
+    { name: "gibbon", img: "images/gibbon.JPG", },
+    { name: "gibbon", img: "images/gibbon.JPG", },
+    { name: "reptile", img: "images/reptile.JPG", },
+    { name: "reptile", img: "images/reptile.JPG", },
     ]; 
     
     //define variables and get DOM element
-    
+
     let grid = document.querySelector(".grid"); 
     let scoreBoard = document.querySelector(".scoreBoard"); 
     let popup = document.querySelector(".popup"); 
@@ -27,89 +32,98 @@ let cardArray = [
     let clicks = 0;
     
 
-    createBoard(grid, cardArray); 
-    arrangeCard();
-    playAgain.addEventListener("click", replay); 
+  
+    document.addEventListener("DOMContentLoaded", function () {
+        let clickBoard = document.querySelector(".clickBoard"); 
+        let scoreBoard = document.querySelector(".scoreBoard"); 
+      
 
-    //add a click function for images 
-
-    imgs = document.querySelectorAll("img");
-    Array.from(imgs).forEach(img => 
-    img.addEventListener("click", flipCard)
-    ) 
+        //define functions 
+        
+        createBoard(grid, cardArray); 
+        arrangeCard();
+        document.querySelector(".playAgain").addEventListener("click", replay); 
+        
+        //add a click function for images 
+        
+        imgs = document.querySelectorAll("img");
+        Array.from(imgs).forEach(img => 
+        img.addEventListener("click", flipCard)
+        ) 
     });
 
     //createBoard function
 
-function createBoard(grid, array) { 
-    popup.style.display = "none"; 
-    array.forEach((arr, index) => { 
-    let img = document.createElement("img"); 
-    img.setAttribute("src", "images/background.JPG");
-    img.setAttribute("data-id", index); 
-    grid.appendChild(img); 
-    })
+    function createBoard(grid, array) { 
+        console.log(array);
+        document.querySelector(".popup").style.display = 'none'; 
+        array.forEach((arr, index) => { 
+        let img = document.createElement("img"); 
+        img.setAttribute("src", "images/background.JPG");
+        img.setAttribute("data-id", index); 
+        document.querySelector(".grid").appendChild(img); 
+        })
     }
     
     // arrangeCard function
     
     function arrangeCard() { 
-    cardArray.sort(() => 0.5 - Math.random())
+        cardArray.sort(() => 0.5 - Math.random())
     }
     
     // flip Card function
     
     function flipCard() { 
-    let selected = this.dataset.id;
-    cardsSelected.push(cardArray[selected].name); 
-    cardsId.push(selected); 
-    this.classList.add("flip"); 
-    this.setAttribute("src", cardArray[selected].img); 
-    if (cardsId.length === 2) { 
-    setTimeout(checkForMatch, 500);
-    } 
+        let selected = this.dataset.id;
+        cardsSelected.push(cardArray[selected].name); 
+        cardsId.push(selected); 
+        this.classList.add("flip"); 
+        this.setAttribute("src", cardArray[selected].img); 
+        if (cardsId.length === 2) { 
+        setTimeout(checkForMatch, 500);
+        } 
     }
 
     // checkForMatch function
     
     function checkForMatch() { 
-    let imgs = document.querySelectorAll("img"); 
-    let firstCard = cardsId[0];
-    let secondCard = cardsId[1];
-    if (cardsSelected[0] === cardsSelected[1] && firstCard !== secondCard) { 
-    alert("you have found a match"); 
-    cardsWon += 1; 
-    scoreBoard.innerHTML = cardsWon; 
-    setTimeout(checkWon,500) 
-    } else { 
-    imgs[firstCard].setAttribute("src", "images/background.JPG");
-    imgs[secondCard].setAttribute("src", "images/background.JPG"); 
-    alert("wrong, try again"); imgs[firstCard].classList.remove("flip"); 
-    imgs[secondCard].classList.remove("flip"); 
+        let imgs = document.querySelectorAll("img"); 
+        let firstCard = cardsId[0];
+        let secondCard = cardsId[1];
+        if (cardsSelected[0] === cardsSelected[1] && firstCard !== secondCard) { 
+            alert("you have found a match"); 
+        cardsWon += 1; 
+        scoreBoard.innerHTML = cardsWon; 
+        setTimeout(checkWon,500) 
+        } else { 
+        imgs[firstCard].setAttribute("src", "images/background.JPG");
+        imgs[secondCard].setAttribute("src", "images/background.JPG"); 
+        alert("wrong, try again"); imgs[firstCard].classList.remove("flip"); 
+        imgs[secondCard].classList.remove("flip"); 
     } 
     cardsSelected = []; 
     cardsId = []; 
     clicks += 1; 
-    clickBoard.innerHTML = clicks; 
+    document.querySelector(".clickBoard").innerHTML = clicks; 
     }
     
     function checkWon() {
-    if (cardsWon == cardArray.length / 2) {
-    alert("You won") 
-    setTimeout(()=> popup.style.display = "flex" ,300); 
-    }
+        if (cardsWon == cardArray.length / 2) {
+        alert("You won") 
+        setTimeout(()=> popup.style.display = "flex" ,300); 
+        }
     }
 
 
     // The replay function
 
-function replay() { 
-    arrangeCard(); 
-    grid.innerHTML = "";
-    createBoard(grid, cardArray);
-    cardsWon = 0;
-    clicks = 0; 
-    clickBoard.innerHTML = 0; 
-    scoreBoard.innerHTML = 0; 
-    popup.style.display = "none"; 
+    function replay() { 
+        arrangeCard(); 
+        grid.innerHTML = "";
+        createBoard(grid, cardArray);
+        cardsWon = 0;
+        clicks = 0; 
+        clickBoard.innerHTML = 0; 
+        scoreBoard.innerHTML = 0; 
+        popup.style.display = "none"; 
     }
